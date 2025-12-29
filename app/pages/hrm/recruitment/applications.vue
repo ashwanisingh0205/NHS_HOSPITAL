23951b6acaec8d6c8d02820f5376ae5f13f11880
Charan Kamal Singh <charankamal@nxtdesigns.com>
2025-12-29 15:29:13 +0530

    fix hrm

---
+++ app/pages/hrm/recruitment/applications.vue
@@ -0,0 +1,18 @@
+<!--
+List of applications
+List of interviews
+list of CV new/OLD
+Evaluation
+Move to Employee Register
+-->
+<template>
+    <div>
+        <h1></h1>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'home'
+});
+</script>
